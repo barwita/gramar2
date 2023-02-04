c659797fce2475fa216be9725f4d426d064596ec
djo <djedaiy@gmail.com>
2023-02-04 12:35:27 +0100

    init

--- test2.py
+++ test2.py
@@ -1,10 +1,5 @@
 import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
@@ -15,16 +10,7 @@ class Gramar(GridLayout):
     def __init__(self, **kwargs):
         super(Gramar, self).__init__(**kwargs)
         pass
-        
-    def press(self,instance):
-        name=self.name.text
-        email=self.email.text
-        print(f'votre nom est {name} et votre email est {email}')
-        self.label.text=f'votre nom est {name} et votre email est {email}'
-        self.name.text=""
-        self.email.text=""
-
-
+    
 class Main(App):
     def build(self):
         return Gramar()
